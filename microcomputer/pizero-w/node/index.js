34f44117f4ce125b21059f62cb92c9c58b1fddae
Francois Roseberry <f_roseberry@live.fr>
2024-02-04 19:43:28 +0000

    Make final adjustments after demo-dryrun

--- microcomputer/pizero-w/node/index.js
+++ microcomputer/pizero-w/node/index.js
@@ -1,6 +1,6 @@
 const awsIot = require('aws-iot-device-sdk')
 
-const interval = 5;
+const interval = 3;
 
 /**
  * Sensor type to read from.
@@ -12,7 +12,7 @@ const interval = 5;
  * AM2302: 22
  */
 const sensorType = 22;
-
+let device;
 
 // sensor gpio pin number
 const sensorPin = 4;
@@ -27,6 +27,7 @@ const sensor = {
     );
   },
   read: function () {
+    console.log('Entering sensor.read...');
     const data = sensorLib.read();
     const temperature = data.temperature.toFixed(2);
     const humidity = data.humidity.toFixed(2);
@@ -35,7 +36,7 @@ const sensor = {
       console.log(
         '\tTemperature: ' + temperature + ' °C',
         '\tHumidity: ' + humidity + '%');
-     // TODO : publish to AWS IoT
+      device.publish('YOUR_TOPIC', JSON.stringify({ temperature, humidity }));
     }
     setTimeout(sensor.read, interval * 1000);
   }
@@ -44,13 +45,16 @@ const sensor = {
 if (sensor.initialize()) {
   console.log('Initialized sensor...');
   // TODO replace variables with your values
-  const device = awsIot.device({
-    keyPath: 'PRIVATE_KEY.key',
-    certPath: 'CERTIFICATE.pem',
-    caPath: 'ROOT_CERTIFICATE.pem',
+  device = awsIot.device({
+    keyPath: './YOUR_KEY.private.key',
+    certPath: './YOUR_CERTIFICATE.cert.pem',
+    caPath: './AmazonRootCA1.pem',
     clientId: 'YOUR_THING_NAME',
-    host: 'TOT_ENDPOINT_FOR_YOUR_AWS_ACCOUNT'
+    host: 'a1ykoqoe4qq7d0-ats.iot.us-west-2.amazonaws.com',
+    debug: true
   });
+  console.log('MQTT wrapper created!');
+  console.log(device);
  
   device.on('connect', () => {
     console.log('Connected to AWS IoT');
